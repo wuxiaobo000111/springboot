2e444552cdad42cd740155fcab436c0458454be8
wuxiaobo <2244027504@qq.com>
2018-07-13 21:06:36 +0800

    Initial commit

---
+++ lean/src/test/java/com/bobo/springboot/lean/LeanApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bobo.springboot.lean;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LeanApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
