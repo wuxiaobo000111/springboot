774e8d6c150e14467534f6c57212a4df9841d5e3
wuxiaobo <wuxiaobo@didachuxing.com>
2018-07-14 09:23:28 +0800

    添加备注

--- lean/src/main/java/com/bobo/springboot/lean/LeanApplication.java
+++ lean/src/main/java/com/bobo/springboot/lean/LeanApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LeanApplication {
-
+	//修改
 	public static void main(String[] args) {
 		SpringApplication.run(LeanApplication.class, args);
 	}
